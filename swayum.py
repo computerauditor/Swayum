692ed54682b4e4c01606ada2d9558807471ca150
Dev(ComputerAuditor) <117805200+computerauditor@users.noreply.github.com>
2024-04-22 00:42:15 +0530

    Update swayum.py
    Modified GUI, made sort of a forest theme

--- swayum.py
+++ swayum.py
@@ -1,54 +1,95 @@
-#© Devyansh Rastogi (@computerauditor)
+# © Devyansh Rastogi (@computerauditor)
 import pyautogui
 import tkinter as tk
 from tkinter import messagebox, simpledialog, filedialog
 from tkinter import ttk
 
 class MouseCaptureGUI:
+#----------------------- START OF GUI & THEME-----------------------------------------------------------------------------
     def __init__(self):
+        # Define window size
+        window_width = 1200
+        window_height = 800
+
+        # Create the main window
         self.root = tk.Tk()
-        self.root.title("Swayum")
-        self.root.configure(bg="black")
+        self.root.title("SWAYUM")
+        self.root.geometry(f"{window_width}x{window_height}")
 
-        self.capture_button = tk.Button(self.root, text="Start Capturing", command=self.start_capturing, fg="yellow",
-                                        bg="black")
-        self.capture_button.pack()
+        # Define button font and size
+        button_font = ("Arial", 14, "bold")
+        button_width = 20
 
-        self.stop_button = tk.Button(self.root, text="Stop Capturing", command=self.stop_capturing, fg="yellow",
-                                     bg="black")
-        self.stop_button.pack()
-        self.stop_button.config(state=tk.DISABLED)
+        # Define frame colors
+        background_color = "#006400"  # Light green for forest background
+        button_bg_color = "#B4D8B4"   # Lighter green for buttons
+        menu_color = "#003300"        # Dark green for menu
+        right_color = "#FFFFFF"
 
-        self.execute_button = tk.Button(self.root, text="Execute", command=self.execute_positions, fg="yellow",
-                                        bg="black")
-        self.execute_button.pack()
+        # Create left frame for buttons
+        left_frame = tk.Frame(self.root, width=200, height=window_height, bg=background_color)
+        left_frame.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Create separator line
+        separator = tk.Frame(self.root, width=2, height=window_height, bg="black")
+        separator.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Create title label with dark green color
+        title_label = tk.Label(left_frame, text="SWAYUM || स्वयंम ||", font=("Arial", 20, "bold"), bg=menu_color, fg="white")
+        title_label.pack(pady=50)
+
+        # Create bottom right label with dark green color
+        bottom_label = tk.Label(self.root, text="Made with ♥ by Dev", font=("Arial", 10, "bold"), bg=button_bg_color, fg=menu_color)
+        bottom_label.pack(side=tk.RIGHT, anchor=tk.SE, padx=20, pady=10)
+
+        # START CAPTURE BUTTON
+        self.capture_button = tk.Button(left_frame, text="Start Capture", font=button_font, command=self.start_capturing, 
+            width=button_width, bg=button_bg_color)
+        self.capture_button.pack(pady=5)
+
+        # STOP CAPTURE BUTTON
+        self.stop_button = tk.Button(left_frame, text="Stop Capture", font=button_font, command=self.stop_capturing, 
+            width=button_width, bg=button_bg_color)
+        self.stop_button.pack(pady=5)
+        self.stop_button.config(state=tk.DISABLED) # INITIAL STATE IS DISABLED 
+
+        # EXECUTE BUTTON
+        self.execute_button = tk.Button(left_frame, text="Execute", command=self.execute_positions, font=button_font,
+            width=button_width, bg=button_bg_color)
+        self.execute_button.pack(pady=5)
         self.execute_button.config(state=tk.DISABLED)
 
-        self.save_button = tk.Button(self.root, text="Save Positions", command=self.save_positions, fg="yellow",
-                                     bg="black")
-        self.save_button.pack()
+        # SAVE WORKFLOW BUTTON
+        self.save_button = tk.Button(left_frame, text="Save Workflow", command=self.save_positions, font=button_font,
+            width=button_width, bg=button_bg_color)
+        self.save_button.pack(pady=5)
         self.save_button.config(state=tk.DISABLED)
 
-        self.load_button = tk.Button(self.root, text="Load Positions", command=self.load_positions, fg="yellow",
-                                     bg="black")
+        # LOAD BUTTON
+        self.load_button = tk.Button(left_frame, text="Load Workflow", command=self.load_positions,font=button_font,
+            width=button_width, bg=button_bg_color) 
         self.load_button.pack()
         self.load_button.config(state=tk.NORMAL)
 
-        self.preview_button = tk.Button(self.root, text="Preview Positions", command=self.preview_positions, fg="yellow",
-                                        bg="black")
-        self.preview_button.pack()
+        # PREVIEW BUTTON
+        self.preview_button = tk.Button(left_frame, text="Preview Positions", command=self.preview_positions, font=button_font,
+            width=button_width, bg=button_bg_color)
+        self.preview_button.pack(pady=5)
         self.preview_button.config(state=tk.DISABLED)
 
-        self.duration_label = tk.Label(self.root, text="Mouse Speed :", fg="yellow", bg="black")
-        self.duration_label.pack()
-
-        self.duration_slider = ttk.Scale(self.root, from_=0, to=10, orient=tk.HORIZONTAL)
-        self.duration_slider.pack()
+        # MOUSE-SPEED TEXT
+        self.duration_label = tk.Label(left_frame, text="Mouse Speed :", width=35, bg=button_bg_color)
+        self.duration_label.pack(pady=5)
+        # MOUSE-SPEED LABEL
+        self.duration_slider = ttk.Scale(left_frame, from_=0, to=10, orient=tk.HORIZONTAL, length=300)
+        self.duration_slider.pack(pady=5)
 
         self.positions = []
 
         self.root.bind('<Control-i>', self.capture_position)
         self.root.mainloop()
+
+#----------------------- END OF GUI & THEME -----------------------------------------------------------------------------
 
     def start_capturing(self):
         self.capture_button.config(state=tk.DISABLED)
@@ -153,7 +194,7 @@ class MouseCaptureGUI:
             print(f"Performed {action} at: ({x}, {y})")
 
     def show_red_dot(self, x, y):
-        dot_size = 10
+        dot_size = 15
         dot_color = "red"
 
         dot_window = tk.Toplevel(self.root)
@@ -207,7 +248,6 @@ class MouseCaptureGUI:
         preview_window = tk.Toplevel(self.root)
         preview_window.title("Position Preview")
         preview_window.configure(bg="black")
-
         canvas_width = self.root.winfo_screenwidth()
         canvas_height = self.root.winfo_screenheight()
         canvas = tk.Canvas(preview_window, width=canvas_width, height=canvas_height, bg="black")
@@ -217,7 +257,6 @@ class MouseCaptureGUI:
             x, y, action, *extra = position
             canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="red")
             canvas.create_text(x + 10, y, text=str(i + 1), fill="white", anchor="w")
-
         preview_window.mainloop()
 
 if __name__ == '__main__':
