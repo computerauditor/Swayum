702e7e6241f4c9c5f530860e9eaff9d4aa85b737
Dev(ComputerAuditor) <117805200+computerauditor@users.noreply.github.com>
2024-05-09 21:30:26 +0530

    Update SAPSIDI.py

--- SAPSIDI.py
+++ SAPSIDI.py
@@ -6,12 +6,35 @@ import tkinter as tk
 from tkinter import messagebox
 
 #-----------------WARING----------------------------
+# Function to show user agreement dialog
+def show_agreement():
+    agreement_text = ("I agree to RUN this script on my own discretion, "
+                      "the author of this script shall not be held responsible for:\n\n"
+                      "1) Any damage this script causes.\n"
+                      "2) Me getting fired because of this 🔥🔥.\n"
+                      "3) ☢️ Thermonuclear War ☢️.\n"
+                      "4) My Crush Rejecting Me 👀.\n"
+                      "5) I won't touch this code until I know what I am doing.")
+    if messagebox.askyesno("User Agreement", agreement_text):
+        show_warning()
+    else:
+        root.destroy()
+
+# Function to show warning message
+def show_warning():
+    warning_text = ("S.A.P.S.A.D.I ~ Made By Dev!! \n\n"
+                    "1) I'AM AT THE SAP MENU.\n\n"
+                    "2) I HAVE CLOSED ALL THE RUNNING EXCEL FILES.\n\n"
+                    "3) I WON'T BE ABLE TO USE THIS PC 'UNLESS' THIS CODE FINISHES")
+    messagebox.showwarning("Warning", warning_text)
+
+# Create tkinter window
 root = tk.Tk()
-root.withdraw()  
-message = "Please ensure you are at the SAP Menu.\nClose all running Excel files before proceeding.\nDon't touch the code untill you know what you are doing\n\n Made By Devyansh Rastogi"
-messagebox.showwarning("Warning", message)
-root.destroy()
-#-----------------------------------------------------------------
+root.withdraw()
+
+# Show user agreement dialog
+show_agreement()
+#-------------------------------------------------------------      ----
 # Function to read doc numbers from a the .txt file
 def read_doc_numbers(file_path):
     with open(file_path, 'r') as file:
@@ -35,144 +58,164 @@ doc_numbers = read_doc_numbers('docnum.txt')
 wb = openpyxl.load_workbook("sap_details.xlsx")
 ws = wb["Invoice Tracker"]
 
-for i, doc_no in enumerate(doc_numbers, start=2):
-    pyautogui.typewrite(doc_no)  # Corrected variable name
-    pyautogui.press('ENTER')
+try:
+    for i, doc_no in enumerate(doc_numbers, start=2):
+        pyautogui.typewrite(doc_no)  # Corrected variable name
+        pyautogui.press('ENTER')
 
-    # Locate the area on the screen where Invoice Date is displayed
-    
-    # INVOICE DATE
-    pyautogui.moveTo(376, 369, 1)
-    time.sleep(1)
-    pyautogui.click(376, 369)
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'a')
-    time.sleep(1)
-    pyautogui.hotkey('ctrl', 'c')
-    invoice_date = pyperclip.paste()
-    time.sleep(0.2)
-    
-    # REFERENCE
-    pyautogui.click(688, 368)
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'a')
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'c')
-    invoice_number = pyperclip.paste()
-    time.sleep(1)
+        # Locate the area on the screen where Invoice Date is displayed
+        
+        # INVOICE DATE
+        pyautogui.moveTo(376, 369, 1)
+        time.sleep(1)
+        pyautogui.click(376, 369)
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'c')
+        invoice_date = pyperclip.paste()
+        time.sleep(0.2)
+        
+        # REFERENCE NUMBER
+        pyautogui.click(688, 368)
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'c')
+        invoice_number = pyperclip.paste()
+        time.sleep(1)
 
-    # VENDOR CODE
-    pyautogui.click(316, 316) # GO TO DETAILS
-    time.sleep(1)
-    pyautogui.click(776, 440) # GO TO INV PARTY
-    time.sleep(1)
-    pyautogui.hotkey('ctrl', 'a')
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'c')
-    vendor_number = pyperclip.paste()
-    time.sleep(1)
+        # INVOICE AMOUNT
+        pyautogui.click(346, 446)
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'c')
+        invoice_amount = pyperclip.paste()
+        time.sleep(1)
 
-    # VENDOR CODE
-    pyautogui.click(789, 807) # GO TO PO NUMBER
-    time.sleep(1)
-    pyautogui.hotkey('ctrl', 'a')
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'c')
-    PO_number = pyperclip.paste()
-    time.sleep(1)
-    
-    # ITEM CODE
-    pyautogui.doubleClick(789, 807)
-    time.sleep(1)
-    pyautogui.moveTo(402, 374)
-    pyautogui.click(402, 374)
-    pyautogui.hotkey('ctrl', 'a')
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'c')
-    item_Code = pyperclip.paste()
+        # # GO TO TDS SECTION
+        # pyautogui.click(516, 303)
+        # time.sleep(5)
 
-    # GRN QUANTITY
+        # VENDOR CODE
+        pyautogui.click(316, 316) # GO TO DETAILS
+        time.sleep(1)
+        pyautogui.click(776, 440) # GO TO INV PARTY
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'c')
+        vendor_number = pyperclip.paste()
+        time.sleep(1)
 
-    pyautogui.moveTo(475,643)
-    time.sleep(2)
-    pyautogui.click(475,643)
-    pyautogui.click(475,643)
-    time.sleep(1)
-    pyautogui.click(515,675)
-    time.sleep(1)
-    pyautogui.moveTo(229,379)
-    pyautogui.click(229,379)
-    time.sleep(1)
-    pyautogui.click(366,643)
-    time.sleep(1)
-    pyautogui.click(406,741)
-    time.sleep(8)
-    pyautogui.click(709,604)
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'f')
-    pyautogui.typewrite('Tr./Ev. Goods receipt')
-    pyautogui.press('ENTER')
-    pyautogui.press('ENTER')
-    for _ in range(10):
+        # VENDOR CODE
+        pyautogui.click(789, 807) # GO TO PO NUMBER
+        time.sleep(2)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'c')
+        PO_number = pyperclip.paste()
+        time.sleep(1)
+        
+        # ITEM CODE
+        pyautogui.doubleClick(789, 807)
+        time.sleep(2)
+        pyautogui.moveTo(380, 369)
+        pyautogui.click(380, 369)
+        pyautogui.hotkey('ctrl', 'a')
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'c')
+        item_Code = pyperclip.paste()
+
+        # GRN QUANTITY
+
+        pyautogui.moveTo(475,643)
+        time.sleep(2)
+        pyautogui.click(475,643)
+        pyautogui.click(475,643)
+        time.sleep(1)
+        pyautogui.click(515,675)
+        time.sleep(1)
+        pyautogui.moveTo(229,379)
+        pyautogui.click(229,379)
+        time.sleep(1)
+        pyautogui.click(384,639)
+        time.sleep(1)
+        pyautogui.click(406,741)
+        time.sleep(8)
+        pyautogui.click(714,657) # Excel Click in the middle to search previously(709,604)
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'f')
+        pyautogui.typewrite('Tr./Ev. Goods receipt')
+        pyautogui.press('ENTER')
+        pyautogui.press('ENTER')
+        for _ in range(10):
+            pyautogui.press('TAB')
+        pyautogui.press('ENTER')
+        time.sleep(1)
         pyautogui.press('TAB')
-    pyautogui.press('ENTER')
-    time.sleep(1)
-    pyautogui.press('TAB')
-    pyautogui.hotkey('ctrl', 'c')
-    GRN_qty = pyperclip.paste()
+        pyautogui.hotkey('ctrl', 'c')
+        GRN_qty = pyperclip.paste()
 
-    # INV_RECEIPT QUANTITY
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'f')
-    pyautogui.typewrite('Tr./Ev. Invoice receipt')
-    pyautogui.press('ENTER')
-    pyautogui.press('ENTER')
-    for _ in range(10):
+        # INV_RECEIPT QUANTITY
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'f')
+        pyautogui.typewrite('Tr./Ev. Invoice receipt')
+        pyautogui.press('ENTER')
+        pyautogui.press('ENTER')
+        for _ in range(10):
+            pyautogui.press('TAB')
+        pyautogui.press('ENTER')
+        time.sleep(1)
         pyautogui.press('TAB')
-    pyautogui.press('ENTER')
-    time.sleep(1)
-    pyautogui.press('TAB')
-    pyautogui.hotkey('ctrl', 'c')
-    InvoiceRec_qty = pyperclip.paste()    
+        pyautogui.hotkey('ctrl', 'c')
+        InvoiceRec_qty = pyperclip.paste()    
 
-    # PARK QUANTITY
-    time.sleep(0.5)
-    pyautogui.hotkey('ctrl', 'f')
-    pyautogui.typewrite('Tr./Ev. Parked invoice')
-    pyautogui.press('ENTER')
-    pyautogui.press('ENTER')
-    for _ in range(10):
+        # PARK QUANTITY
+        time.sleep(0.5)
+        pyautogui.hotkey('ctrl', 'f')
+        pyautogui.typewrite('Tr./Ev. Parked invoice')
+        pyautogui.press('ENTER')
+        pyautogui.press('ENTER')
+        for _ in range(10):
+            pyautogui.press('TAB')
+        pyautogui.press('ENTER')
+        time.sleep(1)
         pyautogui.press('TAB')
-    pyautogui.press('ENTER')
-    time.sleep(1)
-    pyautogui.press('TAB')
-    pyautogui.hotkey('ctrl', 'c')
-    Park_qty = pyperclip.paste()   
+        pyautogui.hotkey('ctrl', 'c')
+        Park_qty = pyperclip.paste()   
 
-    pyautogui.hotkey('alt', 'f4')
-    pyautogui.press('TAB')
-    time.sleep(1)
-    pyautogui.press('ENTER')
-    time.sleep(2)
+        pyautogui.hotkey('alt', 'f4')
+        pyautogui.press('TAB')
+        time.sleep(1)
+        pyautogui.press('ENTER')
+        time.sleep(2)
 
-    ws[f'D{i}'] = doc_no
-    ws[f'F{i}'] = invoice_date
-    ws[f'E{i}'] = invoice_number
-    ws[f'H{i}'] = vendor_number
-    ws[f'L{i}'] = PO_number
-    ws[f'N{i}'] = item_Code
-    ws[f'T{i}'] = GRN_qty
-    ws[f'U{i}'] = InvoiceRec_qty
-    ws[f'V{i}'] = Park_qty
+        # Update Excel file
+        ws[f'A{i}'] = doc_no
+        ws[f'B{i}'] = invoice_date
+        ws[f'C{i}'] = invoice_number
+        ws[f'D{i}'] = vendor_number
+        ws[f'E{i}'] = PO_number
+        ws[f'F{i}'] = invoice_amount
+        ws[f'G{i}'] = item_Code
+        ws[f'H{i}'] = GRN_qty
+        ws[f'I{i}'] = InvoiceRec_qty
+        ws[f'J{i}'] = Park_qty
+        
+        # Save the Excel file
+        wb.save(excel_file)
 
-    pyautogui.moveTo(168, 79)
-    time.sleep(0.5)
-    pyautogui.click(168, 79)    
-    time.sleep(1)
-    pyautogui.click(168, 79)
-    pyautogui.typewrite('/nMIR4')
-    pyautogui.press('ENTER')
-    time.sleep(5)
+        pyautogui.moveTo(168, 79)
+        time.sleep(0.5)
+        pyautogui.click(168, 79)    
+        time.sleep(1)
+        pyautogui.click(168, 79)
+        pyautogui.typewrite('/nMIR4')
+        pyautogui.press('ENTER')
+        time.sleep(8)
 
-# Save the Excel file
-wb.save(excel_file)
+    messagebox.showinfo("Success", "Script completed successfully. Data saved to Excel file.")
+except Exception as e:
+    messagebox.showerror("Error", f"Script failed due to system error:\n{str(e)}")
